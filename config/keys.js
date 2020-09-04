e0da6701133e591b029b12d9f951739926b7bee1
Akshay Raveendran <akshayraveendran1999@gmail.com>
2020-09-05 00:52:11 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const URI = 'mongodb+srv://dbUser:defaultitis@cluster0-bvuoi.mongodb.net/test?retryWrites=true&w=majority'
+const URI = '*****'
 
 const connectDB = async() =>{
 		await mongoose.connect(URI,{ useUnifiedTopology: true,useNewUrlParser: true } );
